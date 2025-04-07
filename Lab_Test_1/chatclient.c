d73f91052096e2c9ea8f46a6a7f1f595368adc23
Shivam08IITKGP <shivam08@kgpian.iitkgp.ac.in>
2025-04-07 18:25:25 +0530

    Lab test codes working properly

--- Lab_Test_1/chatclient.c
+++ Lab_Test_1/chatclient.c
@@ -2,116 +2,97 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <sys/select.h>
-
+#include <arpa/inet.h>
+#include <ctype.h>
 
 #define BUFFER_SIZE 1024
 
-int main(int argc, char *argv[])
+int main()
 {
     int sockfd;
     struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_size;
-    fd_set readfds, tempfds;
-    int maxfd, activity;
+    socklen_t len = sizeof(client_addr);
     char buffer[BUFFER_SIZE];
-
-    // Create client socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
+    // Create socket
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        perror("Error in socket creation");
-        exit(1);
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    // Configure server address to connect to
+    // Configure server address
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-
-    // If server IP provided as argument, use it, otherwise use localhost
-    if (argc > 1)
-    {
-        server_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    }
-    else
-    {
-        server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    }
+    server_addr.sin_port = htons(5000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     // Connect to server
     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         perror("Connection failed");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     // Get client address info
-    addr_size = sizeof(client_addr);
-    getsockname(sockfd, (struct sockaddr *)&client_addr, &addr_size);
+    getsockname(sockfd, (struct sockaddr *)&client_addr, &len);
     printf("Connected to server as %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-    // Initialize file descriptor set
-    FD_ZERO(&readfds);
-    FD_SET(sockfd, &readfds);
-    FD_SET(STDIN_FILENO, &readfds); // Add STDIN to listen for user input
-
-    maxfd = sockfd > STDIN_FILENO ? sockfd : STDIN_FILENO;
+    fd_set readfds;
+    int maxfd = (sockfd > STDIN_FILENO) ? sockfd : STDIN_FILENO;
 
     while (1)
     {
-        // Copy file descriptor set
-        tempfds = readfds;
-
-        // Wait for activity on file descriptors
-        activity = select(maxfd + 1, &tempfds, NULL, NULL, NULL);
-
-        if (activity < 0)
+        FD_ZERO(&readfds);
+        FD_SET(sockfd, &readfds);
+        FD_SET(STDIN_FILENO, &readfds);
+        if (select(maxfd + 1, &readfds, NULL, NULL, NULL) < 0)
         {
             perror("Select error");
-            exit(1);
+            exit(EXIT_FAILURE);
         }
 
-        // Check for input from user
-        if (FD_ISSET(STDIN_FILENO, &tempfds))
+        // Handle user input
+        if (FD_ISSET(STDIN_FILENO, &readfds))
         {
             memset(buffer, 0, BUFFER_SIZE);
-            if (fgets(buffer, BUFFER_SIZE, stdin) != NULL)
+            if (!fgets(buffer, BUFFER_SIZE, stdin))
+                continue;
+            buffer[strcspn(buffer, "\n")] = 0;
+
+            // Validate numeric input
+            bool valid = true;
+            for (int i = 0; buffer[i]; i++)
             {
-                // Remove newline character
-                buffer[strcspn(buffer, "\n")] = 0;
+                if (!isdigit(buffer[i]))
+                {
+                    valid = false;
+                    break;
+                }
+            }
 
-                // Send number to server
+            if (valid)
+            {
                 send(sockfd, buffer, strlen(buffer), 0);
-
-                // Print message
-                printf("Client %s:%d Number %s sent to server\n",
-                       inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buffer);
+                printf("Client %s:%d Number %s sent to server\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buffer);
             }
         }
 
-        // Check for message from server
-        if (FD_ISSET(sockfd, &tempfds))
+        // Handle server messages
+        if (FD_ISSET(sockfd, &readfds))
         {
             memset(buffer, 0, BUFFER_SIZE);
-            int valread = read(sockfd, buffer, BUFFER_SIZE);
-
+            int valread = recv(sockfd, buffer, BUFFER_SIZE, 0);
             if (valread <= 0)
             {
-                // Server disconnected
                 printf("Server disconnected\n");
-                close(sockfd);
-                exit(0);
+                break;
             }
-            else
-            {
-                // Print received message
-                printf("Client: Received the following message text from the server: %s\n", buffer);
-            }
+            printf("Client: Received the following message text from the server: \n%s\n", buffer);
         }
     }
 
--- Lab_Test_1/chatserver.c
+++ Lab_Test_1/chatserver.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/select.h>
+#include <limits.h>
+#include <unistd.h>
 
 #define MAX_CLIENTS 5
 #define BUFFER_SIZE 1024
 
-int main() {
+int main()
+{
     int serverfd, clientsockfd[MAX_CLIENTS];
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_size;
@@ -20,162 +22,194 @@ int main() {
     int numclient = 0;
     int round = 1;
     char buffer[BUFFER_SIZE];
-    
+
     int client_sent[MAX_CLIENTS] = {0};
     int client_numbers[MAX_CLIENTS] = {0};
-    
-    for (i = 0; i < MAX_CLIENTS; i++) {
+
+    for (i = 0; i < MAX_CLIENTS; i++)
+    {
         clientsockfd[i] = 0;
     }
-    
-    serverfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverfd < 0) {
-        perror("Error in socket creation");
-        exit(1);
+
+    // Create server socket
+    if ((serverfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
-    
+
+    // Set socket options
     int opt = 1;
-    if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+    if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
         perror("setsockopt failed");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
-    
-    memset(&server_addr, 0, sizeof(server_addr));
+
+    // Configure server address
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(8080);
-    
-    if (bind(serverfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    server_addr.sin_port = htons(5000);
+
+    // Bind socket
+    if (bind(serverfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         perror("Bind failed");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
-    
-    if (listen(serverfd, 5) < 0) {
+
+    // Listen for connections
+    if (listen(serverfd, 5) < 0)
+    {
         perror("Listen failed");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
-    
+
     printf("Server started. Waiting for connections...\n");
-    
+
     FD_ZERO(&readfds);
     FD_SET(serverfd, &readfds);
     maxfd = serverfd;
-    
-    while(1) {
+
+    while (1)
+    {
         tempfds = readfds;
-        
         activity = select(maxfd + 1, &tempfds, NULL, NULL, NULL);
-        
-        if (activity < 0) {
+
+        if (activity < 0)
+        {
             perror("Select error");
-            exit(1);
+            exit(EXIT_FAILURE);
         }
-        
-        if (FD_ISSET(serverfd, &tempfds)) {
-            int new_socket;
+
+        // Handle new connections
+        if (FD_ISSET(serverfd, &tempfds))
+        {
             addr_size = sizeof(client_addr);
-            new_socket = accept(serverfd, (struct sockaddr *)&client_addr, &addr_size);
-            
-            if (new_socket < 0) {
-                perror("Accept failed");
-                exit(1);
-            }
-            
-            printf("Server: Received a new connection from client %s:%d\n", 
-                   inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-            
-            if (numclient < MAX_CLIENTS) {
+            int new_socket = accept(serverfd, (struct sockaddr *)&client_addr, &addr_size);
+
+            printf("Server: Received a new connection from client %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+            if (numclient < MAX_CLIENTS)
+            {
                 clientsockfd[numclient] = new_socket;
-                numclient++;
-                
                 FD_SET(new_socket, &readfds);
-                if (new_socket > maxfd) {
+                if (new_socket > maxfd)
                     maxfd = new_socket;
-                }
-                
-                // Fixed: Only send prompt when minimum clients are connected
-                if (numclient >= 2) {
-                    sprintf(buffer, "Server: Send your number for Round %d", round);
-                    if (numclient == 2) {
-                        // Send to all existing clients when reaching minimum threshold
-                        for (int j = 0; j < numclient; j++) {
+                numclient++;
+
+                // Send initial message if minimum clients reached
+                if (numclient >= 2)
+                {
+                    snprintf(buffer, BUFFER_SIZE, "Server: Send your number for Round %d", round);
+                    if (numclient == 2)
+                        for (int j = 0; j < numclient; j++)
+                        {
                             send(clientsockfd[j], buffer, strlen(buffer), 0);
                         }
-                    } else {
-                        // Send only to new client for existing rounds
-                        send(new_socket, buffer, strlen(buffer), 0);
+                    else
+                    {
+                        send(clientsockfd[numclient - 1], buffer, strlen(buffer), 0);
                     }
                 }
-            } else {
+            }
+            else
+            {
                 close(new_socket);
             }
         }
-        
-        for (i = 0; i < numclient; i++) {
-            if (FD_ISSET(clientsockfd[i], &tempfds)) {
+
+        // Handle client messages
+        for (i = 0; i < numclient; i++)
+        {
+            if (FD_ISSET(clientsockfd[i], &tempfds))
+            {
                 memset(buffer, 0, BUFFER_SIZE);
                 valread = read(clientsockfd[i], buffer, BUFFER_SIZE);
-                
-                if (valread <= 0) {
-                    getpeername(clientsockfd[i], (struct sockaddr*)&client_addr, &addr_size);
-                    printf("Client %s:%d disconnected\n", 
-                           inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-                    
+
+                if (valread <= 0)
+                {
+                    // Handle client disconnect
+                    getpeername(clientsockfd[i], (struct sockaddr *)&client_addr, &addr_size);
+                    printf("Client %s:%d disconnected\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
                     close(clientsockfd[i]);
                     FD_CLR(clientsockfd[i], &readfds);
-                    
-                    for (int j = i; j < numclient - 1; j++) {
-                        clientsockfd[j] = clientsockfd[j+1];
-                        client_sent[j] = client_sent[j+1];
-                        client_numbers[j] = client_numbers[j+1];
+
+                    // Shift remaining clients
+                    for (int j = i; j < numclient - 1; j++)
+                    {
+                        clientsockfd[j] = clientsockfd[j + 1];
+                        client_sent[j] = client_sent[j + 1];
+                        client_numbers[j] = client_numbers[j + 1];
                     }
                     numclient--;
                     i--;
-                } else {
-                    if (numclient < 2) {
-                        getpeername(clientsockfd[i], (struct sockaddr*)&client_addr, &addr_size);
-                        printf("Server: Insufficient clients, \"%s\" from client %s:%d dropped\n", 
-                               buffer, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-                    } else {
-                        if (client_sent[i]) {
-                            sprintf(buffer, "Server: Duplicate messages for Round %d are not allowed. Please wait for the results for Round %d and Call for the number for Round %d.", 
-                                    round, round, round + 1);
+                }
+                else
+                {
+                    if (numclient < 2)
+                    {
+                        getpeername(clientsockfd[i], (struct sockaddr *)&client_addr, &addr_size);
+                        printf("Server: Insufficient clients, \"%s\" from client %s:%d dropped\n", buffer, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                    }
+                    else
+                    {
+                        if (client_sent[i])
+                        {
+                            snprintf(buffer, BUFFER_SIZE, "Server: Duplicate messages for Round %d are not allowed. Please wait for the results for Round %d and Call for the number for Round %d.", round, round, round + 1);
                             send(clientsockfd[i], buffer, strlen(buffer), 0);
-                        } else {
-                            int num = atoi(buffer);
-                            client_numbers[i] = num;
+                        }
+                        else
+                        {
+                            // Store the number
+                            printf("Server: Received number %s from client %s:%d\n", buffer, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                            client_numbers[i] = atoi(buffer);
                             client_sent[i] = 1;
-                            
+
+                            // Check if all clients have sent numbers
                             int all_sent = 1;
-                            for (int j = 0; j < numclient; j++) {
-                                if (!client_sent[j]) {
+                            for (int j = 0; j < numclient; j++)
+                            {
+                                if (!client_sent[j])
+                                {
                                     all_sent = 0;
                                     break;
                                 }
                             }
-                            
-                            if (all_sent) {
-                                int max_num = client_numbers[0];
-                                int max_client = 0;
-                                for (int j = 1; j < numclient; j++) {
-                                    if (client_numbers[j] > max_num) {
+
+                            if (all_sent)
+                            {
+                                // Find maximum number
+                                int max_num = INT_MIN;
+                                int max_index = 0;
+                                for (int j = 0; j < numclient; j++)
+                                {
+                                    if (client_numbers[j] > max_num)
+                                    {
                                         max_num = client_numbers[j];
-                                        max_client = j;
+                                        max_index = j;
                                     }
                                 }
-                                
+
+                                // Get winner's address
                                 struct sockaddr_in winner_addr;
-                                addr_size = sizeof(winner_addr);
-                                getpeername(clientsockfd[max_client], (struct sockaddr*)&winner_addr, &addr_size);
-                                
-                                sprintf(buffer, "Server: Maximum Number Received in Round %d is: %d. The number has been received from the client %s:%d\nServer: Enter the number for Round %d:", 
-                                        round, max_num, inet_ntoa(winner_addr.sin_addr), ntohs(winner_addr.sin_port), round + 1);
-                                
-                                for (int j = 0; j < numclient; j++) {
+                                socklen_t len = sizeof(winner_addr);
+                                getpeername(clientsockfd[max_index], (struct sockaddr *)&winner_addr, &len);
+
+                                // Prepare message
+                                snprintf(buffer, BUFFER_SIZE,
+                                         "Server: Maximum Number Received in Round %d is: %d. The number has been received from the client %s:%d\n"
+                                         "Server: Enter the number for Round %d:",
+                                         round, max_num, inet_ntoa(winner_addr.sin_addr), ntohs(winner_addr.sin_port), round + 1);
+
+                                // Broadcast results
+                                for (int j = 0; j < numclient; j++)
+                                {
                                     send(clientsockfd[j], buffer, strlen(buffer), 0);
                                     client_sent[j] = 0;
                                 }
-                                
+
                                 round++;
                             }
                         }
@@ -184,6 +218,6 @@ int main() {
             }
         }
     }
-    
+
     return 0;
 }
